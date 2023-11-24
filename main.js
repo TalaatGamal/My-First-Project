f99283ec43c921b38910910f99b93a5985bbb79c
TalaatGamal <talatgamal890@gmail.com>
2023-11-25 00:15:44 +0200

    first commit

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 // comment
 
- 
+    alert("Please note that this website is still under development by Engineer Talaat and that it is not the final version يرجى العلم أن هذا الموقع لا يزال قيد التطوير على يد المهندس طلعت وأنه ليس النسخة النهائية");
